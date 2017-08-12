cf8c751ecab6e2617d19497f13268c3fb64abe2e
josegl <jose@Mini-de-Jose.Home>
2017-08-12 20:29:16 +0200

    Script 1 -> show lat and lng

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
             <h1> List examples , map </h1>
         </div>
         <div id="col-md-12">
+            <h2>¡Work whit maps!</h2>
             <ul>
                 <li><a href ="./example1.php">Static map </a> 
                     <p>
@@ -19,9 +20,17 @@
                     <a href ="./example2.php">Static map whit markets </a> : 
                     <p>This map has points in the map</p>
                 </li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>
+                    <a href ="./example3.php">Deliminter zones in the map </a> : 
+                    <p>In this map , we can watch delimiter zones in the map</p>
+                </li>
+            </ul>
+            <div id="col-md-12">
+            <h2> Scripts , ¡They are comming , in our help!</h2>
+            <ul>
+                <li>
+                    <a href ="./script1.php">Show lat and lang </a>
+                </li>
             </ul>
         </div>
     </div>
---
+++ script1.php
@@ -0,0 +1,46 @@
+
+<?php 
+    include('header.php')
+?>
+
+<div class="container">
+    <div id="row">
+        <div class="col-md-12">
+            <h1>Lat and lang in map</H1>
+        </div>
+        <div class="col-md-12">
+            <div class="col-md-6 p-0">
+                <div id="mapEurope">
+                </div>
+            <div>
+            <div class="col-md-6 p-0">
+             <div id="position">
+        </div>
+            <div>
+        <div>
+    </div>
+    
+
+    <script>
+        // Init map
+        // L.map('<id>').setview(latlng center , initialzoom)
+        
+        var mymap = L.map('mapEurope').setView([50,50], 0);
+        
+        // Define conners of the map
+        var bounds = [[0,0], [400,400]]; // corner 1 and conner 2
+        
+        // Init , static map
+        var image = L.imageOverlay('img/map_europe.png', bounds).addTo(mymap);
+       
+        mymap.on('click', function(e) {
+            html = "Lat : " + Math.round(e.latlng.lat*100)/100+ " <br> Lon :" + Math.round(e.latlng.lng*100)/100+"<br>";
+            $('#position').append(html);
+        });
+
+    </script>
+</div>
+
+<?php 
+    include('footer.php')
+?>
