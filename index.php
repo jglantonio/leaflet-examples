76e05e045a7192257e4cd760795c8cad34f9de65
jglantonio <joseantoniogonzalezlopez@gmail.com>
2017-08-19 20:40:34 +0200

    Refactor and new point

--- index.php
+++ index.php
@@ -11,18 +11,22 @@
         <div id="col-md-12">
             <h2>¡Work whit maps!</h2>
             <ul>
-                <li><a href ="./example1.php">Static map </a> 
+                <li><a href ="./examples/example1.php">Static map </a>
                     <p>
                         Example static map leaflet , europe map white
                     </p>
                 </li>
                 <li>
-                    <a href ="./example2.php">Static map whit markets </a> : 
+                    <a href ="./examples/example2.php">Static map whit markets </a> :
                     <p>This map has points in the map</p>
                 </li>
                 <li>
-                    <a href ="./example3.php">Deliminter zones in the map </a> : 
+                    <a href ="./examples/example3.php">Deliminter zones in the map </a> :
                     <p>In this map , we can watch delimiter zones in the map , and add layer dinamicly</p>
+                </li>
+                <li>
+                    <a href ="./examples/example4.php">Delimiter zone for markets</a> :
+                    <p>Can delimiter zones.</p>
                 </li>
             </ul>
         </div>
--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 console.log("mounted");
 var leafletScript = {
-     variables : function(){
-        var _zone = [];
-        getZone = function(){
-            return _zone;
-        }
-        setZone = function(point){
-            return _zone.push(point);
-        }
-     },
-     setPoligon: function(e){
-        var lat = Math.round(e.latlng.lat*100)/100;
-        var lng = Math.round(e.latlng.lng*100)/100;
-        this.variables([lat,lng]);
-     },
-     getPoligon: function(e){
-         var variable = this.variables.getZone();
-         return variable;
-     }
-};
+    zone : [],
+    setPointToZone : function(e){
+       this.zone.push(
+           this.roundPoint(e.latlng.lat,100),
+           this.roundPoint(e.latlng.lng,100)
+       );
+    },
+    roundPoint : function(point,value){
+        return Math.round(point*value)/value;
+    },
+    getZone:function (){
+        return this.zone;
+    },
+    setZone:function(zone){
+        this.zone = zone;
+    },
+    showLatLng : function (e,id){
+        var lng = this.roundPoint(e.latlng.lng,100);
+        var lat = this.roundPoint(e.latlng.lat,100);
+        var html = "Lat :"+lng + "<br> Lng :"+lat+"<br> ";
+        $(id).append(html);
+    }
+
+}
