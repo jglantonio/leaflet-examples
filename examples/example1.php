ef9b793c300ac6b7ba7c5a17dc877f8880b5d8c7
jglantonio <joseantoniogonzalezlopez@gmail.com>
2017-08-28 14:24:25 +0200

    Solve fix

---
+++ examples/example1.php
@@ -0,0 +1,28 @@
+<?php
+    include('../header.php')
+?>
+
+<div class="container">
+    <h1>Static map</H1>
+    <div id="mapEurope">
+    </div>
+
+    <script>
+        // Init map
+        // L.map('<id>').setview(latlng center , initialzoom)
+        
+        
+        var mymap = L.map('mapEurope').setView([61.85,224.3], 0);
+        
+        // Define conners of the map
+        var bounds = [[0,0], [400,400]]; // corner 1 and conner 2
+        
+        // Init , static map
+        var image = L.imageOverlay('../img/map_europe.png', bounds).addTo(mymap);
+        
+    </script>
+</div>
+
+<?php
+    include('../footer.php')
+?>
---
+++ examples/example4.php
@@ -0,0 +1,57 @@
+<?php
+    include('../header.php')
+?>
+
+
+<div class="container">
+    <div id="row">
+        <div class="col-md-12">
+            <h1>Delimiter zones</h1>
+            <h2>Europe in ww1</h2>
+            <hr>
+        </div>
+        <div class="col-md-12">
+            <div class="col-md-12 p-0">
+                <div id="mapEurope">
+                </div>
+            <div>
+            <div class="col-md-12 p-0">
+                <div class="col-md-12 p-0">
+                    This is lat lng  , you cicked : 
+                </div>    
+                <div id="position" class="col-md-12 p-0">
+                </div>
+            <div>
+        <div>
+    </div>
+    
+
+    <script>
+        var zones = [];
+        // Init map
+        // L.map('<id>').setview(latlng center , initialzoom)
+        var mymap = L.map('mapEurope').setView([65.51 ,211.11], 2);
+
+        // Define conners of the map
+        var bounds = [[0,0], [400,400]]; // corner 1 and conner 2
+        
+        // Init , static map
+        var image = L.imageOverlay('../img/figure.png', bounds).addTo(mymap);
+
+        var size = zoneTags.length;
+        for(var x = 0 ; x < size ; x++){
+            var polygon = L.polygon(zoneTags[x]).addTo(mymap);
+            polygon.bindPopup(zoneTags[x].toString());
+        }
+
+        mymap.on('click', function(e){
+           leafletScript.setPointToZone(e);
+           zones = leafletScript.getZone();
+           leafletScript.showLatLng(e,"#position");
+        });
+    </script>
+</div>
+
+<?php
+    include('../footer.php')
+?>
